c1358d83937936d770516169b71c54c0ffffcf54
hrafaelveloso <rafaelcostaveloso@gmail.com>
2019-10-05 17:48:08 +0100

    ajuste do padding

--- src/utils/theme/overrides/index.js
+++ src/utils/theme/overrides/index.js
@@ -1,7 +1,9 @@
+import MuiDialogContent from './MuiDialogContent';
 import MuiDialogTitle from './MuiDialogTitle';
 import MuiPaper from './MuiPaper';
 
 export default {
+  MuiDialogContent,
   MuiDialogTitle,
   MuiPaper,
 };
