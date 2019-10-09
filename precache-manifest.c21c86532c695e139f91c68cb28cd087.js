fa3ddd1717977d0335d5d526b37bde00726a27ff
Helder Veloso <rafaelcostaveloso@gmail.com>
2019-10-09 09:32:55 +0100

    Updates

---
+++ precache-manifest.c21c86532c695e139f91c68cb28cd087.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515e13c583edd70ac252c37e79041d16",
+    "url": "/bets-divider/index.html"
+  },
+  {
+    "revision": "825a4aa593a2e3c5e8b3",
+    "url": "/bets-divider/static/js/2.f982502a.chunk.js"
+  },
+  {
+    "revision": "62f39788af931e664776",
+    "url": "/bets-divider/static/js/main.2e984eec.chunk.js"
+  },
+  {
+    "revision": "ab16f293667c1e64eeeb",
+    "url": "/bets-divider/static/js/runtime-main.4181e9d8.js"
+  }
+]);
